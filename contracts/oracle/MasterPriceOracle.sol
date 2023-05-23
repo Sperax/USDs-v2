4ba2c0c1d5f1b5c70da3ecc9a3cc6c2eedc4f045
YashP16 <yash.pitroda786@gmail.com>
2023-05-23 17:29:26 +0530

    feat(contracts): Add initial draft for MasterPriceOracle

---
+++ contracts/oracle/MasterPriceOracle.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MasterPriceOracle is Ownable {
+    struct PriceFeedData {
+        address source;
+        bytes msgData;
+    }
+
+    struct PriceData {
+        uint256 price;
+        uint256 precision;
+    }
+
+    // Handles price feed data for a give token.
+    mapping(address => PriceFeedData) public tokenPriceFeed;
+
+    event PriceFeedUpdated(
+        address indexed token,
+        address indexed source,
+        bytes msgData
+    );
+    event PriceFeedRemoved(address indexed token);
+    error UnableToFetchPriceFeed(address token);
+    error InvalidPriceFeed(address token);
+    error PriceFeedNotFound(address token);
+
+    /// @notice Update/Add price feed for `_token`
+    /// @param _token address of the desired token.
+    /// @dev Have to be extra cautious while updating the price feed.
+    /// @param _source price feed source.
+    /// @param _data call data for fetching the price feed.
+    function updateTokenPriceFeed(
+        address _token,
+        address _source,
+        bytes memory _data
+    ) external onlyOwner {
+        // Validate if the price feed is being emitted correctly.
+        _getPriceFeed(_token, _source, _data);
+
+        tokenPriceFeed[_token] = PriceFeedData(_source, _data);
+        emit PriceFeedUpdated(_token, _source, _data);
+    }
+
+    /// @notice Remove an existing price feed for `token`.
+    /// @param _token Address of the token.
+    function removeTokenPriceFeed(address _token) external onlyOwner {
+        if (tokenPriceFeed[_token].source == address(0))
+            revert PriceFeedNotFound(_token);
+        delete tokenPriceFeed[_token];
+        emit PriceFeedRemoved(_token);
+    }
+
+
+    /// @notice Gets the price feed for `_token`.
+    /// @param _token address of the desired token.
+    /// @dev Function reverts if the price feed does not exists.
+    /// @returns PriceData (uint256 price, uint256 precision).
+    function getPrice(address _token) external view returns (PriceData memory) {
+        PriceFeedData memory data = tokenPriceFeed[_token];
+        return _getPriceFeed(_token, data.source, data.msgData);
+    }
+
+    /// @notice Gets the price feed for a `_token` given the feed data.
+    /// @param _token address of the desired token.
+    /// @param _source price feed source.
+    /// @param _msgData call data for fetching feed.
+    /// @returns PriceData (uint256 price, uint256 precision).
+    function _getPriceFeed(
+        address _token,
+        address _source,
+        bytes memory _msgData
+    ) private view returns (PriceData memory priceData) {
+        if (_source == address(0)) revert InvalidPriceFeed(_token);
+        (bool success, bytes memory response) = _source.staticcall(_msgData);
+        if (success) {
+            priceData = abi.decode(response, (PriceData));
+            return priceData;
+        }
+        revert UnableToFetchPriceFeed(_token);
+    }
+}
