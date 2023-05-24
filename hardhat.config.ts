e04542755d380021d7616b585bebec6432de1b24
YashP16 <yash.pitroda786@gmail.com>
2023-05-24 21:00:17 +0530

    build(hardhat-config): Update Hardhat-config.ts file
    1. Add mainnet and fork-net config
    2. Install foundry plugin.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,36 @@
+import "@foundry-rs/hardhat-forge";
+import "@nomicfoundation/hardhat-foundry";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-solhint";
-import { HardhatUserConfig } from "hardhat/config";
+import { config as dotenvConf } from "dotenv";
+import { task } from "hardhat/config";
+import type {
+  HardhatUserConfig,
+  HttpNetworkUserConfig,
+} from "hardhat/types";
+
+dotenvConf({ path: __dirname + "/.env" });
+
+const { ALCHEMY_KEY, PK, MNEMONIC } = process.env;
+
+const DEFAULT_MNEMONIC = "hardhat";
+
+const sharedNetworkConfig: HttpNetworkUserConfig = {};
+if (PK) {
+  sharedNetworkConfig.accounts = [PK];
+} else {
+  sharedNetworkConfig.accounts = {
+    mnemonic: MNEMONIC || DEFAULT_MNEMONIC,
+  };
+}
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -13,13 +43,17 @@ const config: HardhatUserConfig = {
     },
   },
   networks: {
+    "arbitrum-main": {
+      url: `https://arb-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`,
+      ...sharedNetworkConfig,
+    },
     localhost: {
       url: "http://127.0.0.1:8545",
     },
     hardhat: {
       forking: {
-        url: "",
-        // blocknumber: <blk_no_for_fork>
+        url: `https://arb-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`,
+        blockNumber: 93644011,
       },
     },
   },
