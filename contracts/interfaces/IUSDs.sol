c286ccaa28ece00ede5067d65164f485b1f5e678
YashP16 <yash.pitroda786@gmail.com>
2023-07-13 15:52:29 +0530

    feat(contracts/token): Migrate the USDs contract from older Repo.
    Ref: https://github.com/Sperax/USDs/blob/main/contracts/token/USDsL2.sol (f87711f7272d15483624a0756d1d5c7d2fe942ac)
    
    Changes:
    * Use ERC20PermitUpgradeable instead of aeERC20
    * Remove arbitrum bridge functions
    * Add latest StableMath library from mStable/protocol
    * Rename deprecated variables
    * Reorder functions
    * Remove unused functions

---
+++ contracts/interfaces/IUSDs.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+interface IUSDs {
+    function mint(address _account, uint256 _amount) external;
+
+    function burn(uint256 _amount) external;
+
+    function rebase(uint256 _rebaseAmt) external;
+
+    function totalSupply() external view returns (uint256);
+
+    function nonRebasingSupply() external view returns (uint256);
+}
