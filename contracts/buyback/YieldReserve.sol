0adc187da30257e91f904821a8cada6c06dd64f6
Parv3213 <parv3213@gmail.com>
2023-09-19 12:53:12 +0530

    No need to pre-calculate `_minUSDSAmt` before `mint`
    - `mintView` called from YieldReverse will result in the same value as `mintView` called from VaultCore. Hence, `_minUSDSAmt` can just be passed a 0.

--- contracts/buyback/YieldReserve.sol
+++ contracts/buyback/YieldReserve.sol
@@ -215,16 +215,7 @@ contract YieldReserve is ReentrancyGuard, Ownable {
         if (_srcToken != Helpers.USDS) {
             // Mint USDs
             IERC20(_srcToken).safeApprove(vault, _amountIn);
-            (uint256 _minUSDSAmt, ) = IVault(vault).mintView(
-                _srcToken,
-                _amountIn
-            );
-            IVault(vault).mint(
-                _srcToken,
-                _amountIn,
-                _minUSDSAmt,
-                block.timestamp + 1200
-            );
+            IVault(vault).mint(_srcToken, _amountIn, 0, block.timestamp + 1200);
             // No need to do slippage check as it is our contract
             // and the vault does that.
         }
