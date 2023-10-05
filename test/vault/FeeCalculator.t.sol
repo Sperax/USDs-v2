1d03eaa9b1565529bdbcdf3b48a142f39a78b17c
mehultuteja <arcantheon@gmail.com>
2023-10-05 21:55:40 +0530

    Added calibrate fee test cases

--- test/vault/FeeCalculator.t.sol
+++ test/vault/FeeCalculator.t.sol
@@ -6,26 +6,194 @@ import {Helpers, FeeCalculator} from "../../contracts/vault/FeeCalculator.sol";
 import {ICollateralManager} from "../../contracts/vault/interfaces/ICollateralManager.sol";
 import {IOracle} from "../../contracts/interfaces/IOracle.sol";
 import {IUSDs} from "../../contracts/interfaces/IUSDs.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TestFeeCalculator is PreMigrationSetup {
-    FeeCalculator private feeCalculator;
+contract FeeCalculatorTestSetup is PreMigrationSetup {
+    FeeCalculator internal feeCalculator;
+    ICollateralManager internal collateralManager;
+    address internal _collateral;
+    uint16 internal constant DISCOUNT_FACTOR = 2;
+    uint16 internal constant PENALTY_MULTIPLIER = 2;
+
+    function setUp() public virtual override {
+        super.setUp();
+        feeCalculator = FeeCalculator(FEE_CALCULATOR);
+        collateralManager = ICollateralManager(COLLATERAL_MANAGER);
+        _collateral = USDCe;
+    }
+}
+
+contract TestFeeCalculatorInit is FeeCalculatorTestSetup {
+    function testInitialization() public {
+        address[] memory collaterals = collateralManager.getAllCollaterals();
+        uint256 colLength = collaterals.length;
+        for (uint256 i; i < colLength; ) {
+            (uint32 nextUpdate, , ) = feeCalculator.collateralFee(
+                collaterals[i]
+            );
+            assertTrue(nextUpdate != 0);
+            unchecked {
+                ++i;
+            }
+        }
+    }
+}
+
+contract TestCalibrateFee is FeeCalculatorTestSetup {
+    uint256 availableCollateral;
+
+    function setUp() public override {
+        super.setUp();
+        availableCollateral = 1 * 10 ** ERC20(_collateral).decimals();
+    }
+
+    function test_revertsIf_InvalidCalibration() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(FeeCalculator.InvalidCalibration.selector)
+        );
+        feeCalculator.calibrateFee(_collateral);
+    }
+
+    function test_CalibrateFee_TotalCollateralLTLowerLimit() public {
+        setCollateralData(_collateral);
+        vm.warp(block.timestamp + 1 days);
+        // Desired collateral composition is set as 1000 in setCollateralData()
+        // But in Fee calculator the desired composition would be 500 to 1500 hence
+        // below mocking would return the collateral as 1200 means 12% of TVL
+        // equally divided in vault and strategies
+        mockCollateralCalls(
+            (IUSDs(USDS).totalSupply() * 600) / 10000,
+            (IUSDs(USDS).totalSupply() * 600) / 10000
+        );
+        feeCalculator.calibrateFee(_collateral);
+        uint256 oldFeeIn = feeCalculator.getFeeIn(_collateral);
+        uint256 oldFeeOut = feeCalculator.getFeeOut(_collateral);
+        vm.warp(block.timestamp + 1 days);
+        // Collateral composition calls are mocked to return lesser than lower limit
+        mockCollateralCalls(availableCollateral / 2, availableCollateral / 2);
+        feeCalculator.calibrateFee(_collateral);
+        vm.clearMockedCalls();
+        uint256 newFeeIn = feeCalculator.getFeeIn(_collateral);
+        uint256 newFeeOut = feeCalculator.getFeeOut(_collateral);
+
+        assertEq(oldFeeIn / 2, newFeeIn);
+        assertEq(oldFeeOut * 2, newFeeOut);
+    }
+
+    function test_CalibrateFee_TotalCollateralIsInDesiredRange() public {
+        setCollateralData(_collateral);
+        vm.warp(block.timestamp + 1 days);
+        // Desired collateral composition is set as 1000 in setCollateralData()
+        // But in Fee calculator the desired composition would be 500 to 1500 hence
+        // below mocking would return the collateral as 1200 means 12% of TVL
+        // equally divided in vault and strategies
+        mockCollateralCalls(
+            (IUSDs(USDS).totalSupply() * 600) / 10000,
+            (IUSDs(USDS).totalSupply() * 600) / 10000
+        );
+        feeCalculator.calibrateFee(_collateral);
+        uint256 oldFeeIn = feeCalculator.getFeeIn(_collateral);
+        uint256 oldFeeOut = feeCalculator.getFeeOut(_collateral);
+        vm.warp(block.timestamp + 1 days);
+        // Ratio is changed but still in desired range
+        mockCollateralCalls(
+            (IUSDs(USDS).totalSupply() * 300) / 10000,
+            (IUSDs(USDS).totalSupply() * 300) / 10000
+        );
+        feeCalculator.calibrateFee(_collateral);
+        vm.clearMockedCalls();
+        uint256 newFeeIn = feeCalculator.getFeeIn(_collateral);
+        uint256 newFeeOut = feeCalculator.getFeeOut(_collateral);
+
+        assertEq(oldFeeIn, newFeeIn);
+        assertEq(oldFeeOut, newFeeOut);
+    }
+
+    function test_CalibrateFee_TotalCollateralGTUpperLimit() public {
+        setCollateralData(_collateral);
+        vm.warp(block.timestamp + 1 days);
+        // Desired collateral composition is set as 1000 in setCollateralData()
+        // But in Fee calculator the desired composition would be 500 to 1500 hence
+        // below mocking would return the collateral as 200 means 2% of TVL LT lower limit
+        // equally divided in vault and strategies
+        mockCollateralCalls(
+            (IUSDs(USDS).totalSupply() * 100) / 10000,
+            (IUSDs(USDS).totalSupply() * 100) / 10000
+        );
+        feeCalculator.calibrateFee(_collateral);
+        uint256 oldFeeIn = feeCalculator.getFeeIn(_collateral);
+        uint256 oldFeeOut = feeCalculator.getFeeOut(_collateral);
+        vm.warp(block.timestamp + 1 days);
+        // Collateral composition calls are mocked to return higher than upper limit
+        mockCollateralCalls(
+            (IUSDs(USDS).totalSupply() * 800) / 10000,
+            (IUSDs(USDS).totalSupply() * 800) / 10000
+        );
+        feeCalculator.calibrateFee(_collateral);
+        vm.clearMockedCalls();
+        uint256 newFeeIn = feeCalculator.getFeeIn(_collateral);
+        uint256 newFeeOut = feeCalculator.getFeeOut(_collateral);
+
+        // Basis taken as 4 because the old fee would be LT lower limit
+        // and new fee would be GT higher limit
+        assertEq(oldFeeIn * 4, newFeeIn);
+        assertEq(oldFeeOut / 4, newFeeOut);
+    }
+
+    // @todo add fuzzing test cases
+
+    function setCollateralData(address _collateral) internal {
+        ICollateralManager.CollateralBaseData memory _data = ICollateralManager
+            .CollateralBaseData({
+                mintAllowed: true,
+                redeemAllowed: true,
+                allocationAllowed: true,
+                baseFeeIn: 500,
+                baseFeeOut: 500,
+                downsidePeg: 9800,
+                desiredCollateralComposition: 1000
+            });
+        vm.prank(USDS_OWNER);
+        ICollateralManager(COLLATERAL_MANAGER).updateCollateralData(
+            _collateral,
+            _data
+        );
+    }
+
+    function mockCollateralCalls(
+        uint256 _vaultAmt,
+        uint256 _strategyAmt
+    ) internal {
+        vm.mockCall(
+            COLLATERAL_MANAGER,
+            abi.encodeWithSignature(
+                "getCollateralInVault(address)",
+                _collateral
+            ),
+            abi.encode(_vaultAmt)
+        );
+        vm.mockCall(
+            COLLATERAL_MANAGER,
+            abi.encodeWithSignature(
+                "getCollateralInStrategies(address)",
+                _collateral
+            ),
+            abi.encode(_strategyAmt)
+        );
+    }
+}
+
+contract TestFeeCalculator is FeeCalculatorTestSetup {
     IOracle.PriceData private priceData;
     uint16 private baseFeeIn;
     uint16 private baseFeeOut;
     uint16 private composition;
     uint16 private constant LOWER_THRESHOLD = 5000;
     uint16 private constant UPPER_THRESHOLD = 15000;
-    uint16 private constant DISCOUNT_FACTOR = 2;
-    uint16 private constant PENALTY_MULTIPLIER = 2;
-
-    function setUp() public override {
-        super.setUp();
-        feeCalculator = new FeeCalculator(COLLATERAL_MANAGER);
-    }
 
     function testGetFeeIn() public {
         baseFeeIn = getFeeIn();
-        uint256 feeIn = feeCalculator.getFeeIn(USDCe);
+        uint256 feeIn = feeCalculator.getFeeIn(_collateral);
         assertEq(feeIn, baseFeeIn, "Fee in mismatch");
     }
 
@@ -37,8 +205,8 @@ contract TestFeeCalculator is PreMigrationSetup {
 
     function getFeeIn() private returns (uint16) {
         (baseFeeIn, , composition) = ICollateralManager(COLLATERAL_MANAGER)
-            .getCollateralFeeData(USDCe);
-        uint256 totalCollateral = getTotalCollateral(USDCe);
+            .getCollateralFeeData(_collateral);
+        uint256 totalCollateral = getTotalCollateral(_collateral);
         uint256 tvl = IUSDs(Helpers.USDS).totalSupply();
         uint256 desiredCollateralAmt = (tvl * composition) /
             (Helpers.MAX_PERCENTAGE);
@@ -57,8 +225,8 @@ contract TestFeeCalculator is PreMigrationSetup {
 
     function getFeeOut() private returns (uint16) {
         (, baseFeeOut, composition) = ICollateralManager(COLLATERAL_MANAGER)
-            .getCollateralFeeData(USDCe);
-        uint256 totalCollateral = getTotalCollateral(USDCe);
+            .getCollateralFeeData(_collateral);
+        uint256 totalCollateral = getTotalCollateral(_collateral);
         uint256 tvl = IUSDs(Helpers.USDS).totalSupply();
         uint256 desiredCollateralAmt = (tvl * composition) /
             (Helpers.MAX_PERCENTAGE);
