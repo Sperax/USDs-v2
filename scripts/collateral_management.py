b1e0dc2f33a9c24e26935d3e017c9554a8ca8295
YashP16 <yash.pitroda786@gmail.com>
2024-01-29 15:04:17 +0530

    feat(scripts): Add utility script for getting collateral info

---
+++ scripts/collateral_management.py
@@ -0,0 +1,114 @@
+from .preset import  (
+    usdc,
+    usdc_e,
+    dai,
+    usdt,
+    lusd,
+    frax,
+    usds,
+    aave_strategy,
+    master_price_oracle,
+    compound_strategy,
+    stargate_strategy,
+    vault,
+    collateral_manager
+)
+from .utils import get_user
+
+
+collateral_allocation_data = {
+    usdc: {
+        aave_strategy: 5000,
+        compound_strategy: 5000
+    },
+    usdc_e: {
+        aave_strategy: 5000,
+        compound_strategy: 5000
+    },
+    dai: {
+        aave_strategy: 10000
+    },
+    lusd: {
+        aave_strategy: 10000
+    },
+    usdt: {
+        stargate_strategy: 7000,
+        aave_strategy: 3000
+    },
+    frax: {
+        stargate_strategy: 10000
+    }   
+}
+
+def get_vault_stat():
+    collaterals = [usdc, usdc_e, dai, usdt, lusd, frax]
+    total_in_vault = 0
+    total_val_in_vault = 0
+    total_in_strategies = 0
+    total_val_in_strategies = 0
+    total_supply_usds = usds.totalSupply() / 10**usds.decimals()
+    data = {}
+    for collateral in collaterals:
+        collateral_data = dict()
+        decimals = collateral.decimals()
+        collateral_price, collateral_price_precision = master_price_oracle.getPrice(collateral)
+        collateral_data['price'] = collateral_price / collateral_price_precision
+        collateral_data['collateral_stat'] = get_collateral_stats(collateral)
+        collateral_data['collateral_amt_in_vault'] = collateral_manager.getCollateralInVault(collateral) / 10**decimals
+        collateral_data['collateral_amt_in_strategies'] = collateral_manager.getCollateralInStrategies(collateral) / 10**decimals
+        collateral_data['collateral_val_in_vault'] = (collateral_data['collateral_amt_in_vault'] * collateral_data['price'])
+        collateral_data['collateral_val_in_strategies'] = (collateral_data['collateral_amt_in_strategies'] * collateral_data['price'])
+        data[collateral] = collateral_data        
+        total_val_in_vault += collateral_data['collateral_val_in_vault']
+        total_val_in_strategies += collateral_data['collateral_val_in_strategies']
+        total_in_vault += collateral_data['collateral_amt_in_vault']
+        total_in_strategies += collateral_data['collateral_amt_in_strategies']
+    total_collateral_amt = total_in_vault + total_in_strategies
+    return {
+        'total_amt_in_vault': total_in_vault,
+        'total_in_strategies': total_in_strategies,
+        'total_amount_locked': total_collateral_amt,
+        'total_supply_usds': total_supply_usds,
+        'collateral_ratio': total_collateral_amt / total_supply_usds,
+        'total_val_in_vault': total_val_in_vault,
+        'total_val_in_strategies': total_val_in_strategies,
+        'tvl': total_val_in_strategies + total_val_in_vault,
+        'collateral_data': data
+    }
+
+
+def get_collateral_strategy_stat(collateral, strategy):
+    decimals = collateral.decimals()
+    collateral_in_vault = collateral_manager.getCollateralInVault(collateral)
+    collateral_in_all_strategies = collateral_manager.getCollateralInStrategies(collateral)
+    total_collateral = collateral_in_vault + collateral_in_all_strategies
+    collateral_in_strategy = collateral_manager.getCollateralInAStrategy(collateral, strategy)
+    collateral_cap_for_strategy = collateral_allocation_data[collateral][strategy]
+    allocatable_amt = max(0, ((total_collateral * collateral_cap_for_strategy) / 10000) - collateral_in_strategy)
+    interest_earned = strategy.checkInterestEarned(collateral)
+    reward_earned = strategy.checkRewardEarned()
+    return {
+        'allocatable_amt': allocatable_amt/10**decimals,
+        'collateral_in_strategy': collateral_in_strategy/10**decimals,
+        'collateral_in_vault': collateral_in_vault/10**decimals,
+        'total_collateral': total_collateral/10**decimals,
+        'interest_earned': interest_earned/10**decimals,
+        'reward_earned': reward_earned
+    }
+
+def get_collateral_stats(collateral):
+    collateral_data = dict()
+    for key in collateral_allocation_data[collateral].keys():
+        collateral_data[key] = get_collateral_strategy_stat(collateral, key)
+    return collateral_data          
+
+def allocate(collateral, strategy, amount, user): 
+    vault.allocate(collateral, strategy, amount, {'from': user})
+
+
+def main():
+    owner = get_user('Select deployer ')
+    collaterals = [usdc, usdc_e, usdt, dai, frax, lusd]
+    data = {}
+    for collateral in collaterals:
+        data[collateral] = get_collateral_stats(collateral)
--- scripts/preset.py
+++ scripts/preset.py
@@ -28,53 +28,57 @@ DEPLOYMENT_ARTIFACTS = f'deployed/{network.show_active()}/deployment_data.json'
 MIGRATE = True
 USDs_OWNER = '0x5b12d9846F8612E439730d18E1C12634753B1bF1'
 
+data = {}
+with open(DEPLOYMENT_ARTIFACTS) as file:
+    data = json.load(file)
+
+# Deploy all the ERC20 contracts
+usdc = ERC20.at(data['USDC'])
+usdc_e = ERC20.at(data['USDCe'])
+dai = ERC20.at(data['DAI'])
+usdt = ERC20.at(data['USDT'])
+spa = ERC20.at(data['SPA'])
+frax = ERC20.at(data['FRAX'])
+arb = ERC20.at(data['ARB'])
+lusd = ERC20.at(data['LUSD'])
+
+proxy_admin = Contract.from_abi('ProxyAdmin', data['proxy_admin'], ProxyAdmin.abi)
+usds = Contract.from_abi('USDs', data['usds'], USDs.abi)
+
+chainlink_oracle = Contract.from_abi(
+    'chainlinkOracle', data['chainlink_oracle'], ChainlinkOracle.abi
+)
+master_price_oracle = Contract.from_abi(
+    'MPO', data['master_price_oracle'], MasterPriceOracle.abi
+)
+
+vault = Contract.from_abi('Vault', data['vault'], VaultCore.abi)
+fee_calculator = Contract.from_abi(
+    'FeeCalculator', data['fee_calculator'], FeeCalculator.abi
+)
+collateral_manager = Contract.from_abi(
+    'CM', data['collateral_manager'], CollateralManager.abi
+)
+dripper = Contract.from_abi('Dripper', data['dripper'], Dripper.abi)
+rebase_manager = Contract.from_abi(
+    'RebaseManager', data['rebase_manager'], RebaseManager.abi
+)
+spa_buyback = Contract.from_abi('SPABuyback', data['spa_buyback'], SPABuyback.abi)
+yield_reserve = Contract.from_abi(
+    'YieldReserve', data['yield_reserve'], YieldReserve.abi
+)
+aave_strategy = Contract.from_abi(
+    'AaveStrategy', data['aave_strategy'], AaveStrategy.abi
+)
+stargate_strategy = Contract.from_abi(
+    'StargateStrategy', data['stargate_strategy'], StargateStrategy.abi
+)
+compound_strategy = Contract.from_abi(
+    'CompoundStrategy', data['compound_strategy'], CompoundStrategy.abi
+)
+
 def main():
     owner = get_user('Select deployer ')
-    data = {}
-    with open(DEPLOYMENT_ARTIFACTS) as file:
-        data = json.load(file)
+    
 
-    # Deploy all the ERC20 contracts
-    usdc = ERC20.at(data['USDC'])
-    usdc_e = ERC20.at(data['USDCe'])
-    dai = ERC20.at(data['DAI'])
-    usdt = ERC20.at(data['USDT'])
-    spa = ERC20.at(data['SPA'])
-    frax = ERC20.at(data['FRAX'])
-    arb = ERC20.at(data['ARB'])
-    lusd = ERC20.at(data['LUSD'])
-
-    proxy_admin = Contract.from_abi('ProxyAdmin', data['proxy_admin'], ProxyAdmin.abi)
-    usds = Contract.from_abi('USDs', data['usds'], USDs.abi)
-
-    chainlink_oracle = Contract.from_abi(
-        'chainlinkOracle', data['chainlink_oracle'], ChainlinkOracle.abi
-    )
-    master_price_oracle = Contract.from_abi(
-        'MPO', data['master_price_oracle'], MasterPriceOracle.abi
-    )
-
-    vault = Contract.from_abi('Vault', data['vault'], VaultCore.abi)
-    fee_calculator = Contract.from_abi(
-        'FeeCalculator', data['fee_calculator'], FeeCalculator.abi
-    )
-    collateral_manager = Contract.from_abi(
-        'CM', data['collateral_manager'], CollateralManager.abi
-    )
-    dripper = Contract.from_abi('Dripper', data['dripper'], Dripper.abi)
-    rebase_manager = Contract.from_abi(
-        'RebaseManager', data['rebase_manager'], RebaseManager.abi
-    )
-    spa_buyback = Contract.from_abi('SPABuyback', data['spa_buyback'], SPABuyback.abi)
-    yield_reserve = Contract.from_abi(
-        'YieldReserve', data['yield_reserve'], YieldReserve.abi
-    )
-    aave_strategy = Contract.from_abi(
-        'AaveStrategy', data['aave_strategy'], AaveStrategy.abi
-    )
-    stargate_strategy = Contract.from_abi(
-        'StargateStrategy', data['stargate_strategy'], StargateStrategy.abi
-    )
-    compound_strategy = Contract.from_abi(
-        'CompoundStrategy', data['compound_strategy'], CompoundStrategy.abi
-    )
+    
