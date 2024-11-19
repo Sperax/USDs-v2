697483da8feb846f847f308d75aa50b9e553c219
arcantheon@gmail.com <arcantheon@gmail.com>
2024-11-19 18:00:20 +0530

    Updated interface

--- contracts/strategies/fluid/interfaces/IfToken.sol
+++ contracts/strategies/fluid/interfaces/IfToken.sol
@@ -8,7 +8,13 @@ interface IfToken {
         external
         returns (uint256 shares_);
 
+    function redeem(uint256 shares_, address receiver_, address owner_, uint256 minAmountOut_)
+        external
+        returns (uint256 assets_);
+
     function asset() external view returns (address asset);
 
     function convertToShares(uint256 assets_) external view returns (uint256);
+
+    function convertToAssets(uint256 shares_) external view returns (uint256);
 }
