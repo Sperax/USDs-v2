b43b605e9db1c7f204609501f59834b34086a252
arcantheon@gmail.com <arcantheon@gmail.com>
2024-11-20 19:15:50 +0530

    Implemented all the remaining functionalities

--- contracts/strategies/fluid/interfaces/IfToken.sol
+++ contracts/strategies/fluid/interfaces/IfToken.sol
@@ -6,9 +6,13 @@ interface IfToken {
 
     function redeem(uint256 shares_, address receiver_, address owner_) external returns (uint256 assets_);
 
+    function withdraw(uint256 assets_, address receiver_, address owner_) external returns (uint256 shares_);
+
     function asset() external view returns (address asset);
 
     function previewDeposit(uint256 assets_) external view returns (uint256);
 
     function previewWithdraw(uint256 shares_) external view returns (uint256);
+
+    function convertToAssets(uint256 shares_) external view returns (uint256);
 }
